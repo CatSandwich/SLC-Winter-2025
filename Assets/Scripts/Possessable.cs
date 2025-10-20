24424e79b51fa9ef6ad379a1631f5111049a0bf9
Josh TS <jtorrington2@gmail.com>
2025-10-20 16:12:57 -0400

    Pedestal preview

--- Assets/Scripts/Possessable.cs
+++ Assets/Scripts/Possessable.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Experimental.Rendering;
 
 public class Possessable : MonoBehaviour
 {
     public bool StartPossessed;
+    public Camera PreviewCamera;
     public GameObject[] EnableWhilePossessedObjects;
     public MonoBehaviour[] EnableWhilePossessedComponents;
     public UnityEvent PossessionStarted;
     public UnityEvent PossessionEnded;
     public KeyCode UnpossessKey = KeyCode.Escape;
 
+    public RenderTexture PreviewTexture { get; private set; }
     private Possessable _possessedBy;
 
     public void GetPossessedBy(Possessable from)
@@ -24,6 +27,24 @@ public class Possessable : MonoBehaviour
         PossessionEnded.Invoke();
         _possessedBy.PossessionStarted.Invoke();
         _possessedBy = null;
+    }
+
+    private void Awake()
+    {
+        if (PreviewCamera)
+        {
+            PreviewTexture = new RenderTexture(256, 256, 32, GraphicsFormat.R8G8B8A8_UNorm, 8);
+            PreviewTexture.Create();
+            PreviewCamera.targetTexture = PreviewTexture;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (PreviewCamera)
+        {
+            PreviewCamera.targetTexture.Release();
+        }
     }
 
     private void Start()
--- Assets/Scripts/PossessionPedastal.cs
+++ Assets/Scripts/PossessionPedastal.cs
@@ -2,13 +2,27 @@ using UnityEngine;
 
 public class PossessionPedestal : MonoBehaviour
 {
+    public static Material Material => Instantiate(_material ??= new Material(Shader.Find("Sprites/Default")));
+    private static Material _material;
+
     public Possessable Target;
+    public MeshRenderer Preview;
 
     public void OnInteract(Interactor interactor)
     {
         if (interactor.gameObject.TryGetComponent(out Possessable current))
         {
             Target.GetPossessedBy(current);
+        }
+    }
+
+    private void Start()
+    {
+        if (Target.PreviewCamera)
+        {
+            Preview.material = Material;
+            Preview.material.mainTexture = Target.PreviewCamera.targetTexture;
+            Preview.material.color = new Color(1, 1, 1, .7f);
         }
     }
 }
