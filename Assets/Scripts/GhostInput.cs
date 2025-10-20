8aea29b53575a86de3145b0f6652c84603758f52
Cailin Paris <cailinparis123@gmail.com>
2025-10-20 17:28:23 -0400

    Created Level2Beta

--- Assets/Scripts/GhostInput.cs
+++ Assets/Scripts/GhostInput.cs
@@ -2,16 +2,28 @@ using UnityEngine;
 
 public class GhostInput : MonoBehaviour
 {
+    public float movementSpeed; 
+
     private void Update()
     {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * movementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * movementSpeed * Time.deltaTime;
+        }
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.left * 3 * Time.deltaTime;
+            transform.position += Vector3.left * movementSpeed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * 3 * Time.deltaTime;
+            transform.position += Vector3.right * movementSpeed * Time.deltaTime;
         }
     }
 }
