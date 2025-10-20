b503e7799f560a9a3af6038ec9a885c5edd14fe5
Josh TS <jtorrington2@gmail.com>
2025-10-20 14:00:38 -0400

    Made possession workflow easier

--- Assets/Scripts/PossessionTarget.cs
+++ Assets/Scripts/PossessionTarget.cs
@@ -3,11 +3,29 @@ using UnityEngine.Events;
 
 public class PossessionTarget : MonoBehaviour
 {
+    public GameObject[] EnableWhilePossessedObjects;
+    public MonoBehaviour[] EnableWhilePossessedComponents;
     public UnityEvent PossessionStarted;
     public UnityEvent PossessionEnded;
 
     private void Start()
     {
+        PossessionStarted.AddListener(() => SetWhilePossessedStates(true));
+        PossessionEnded.AddListener(() => SetWhilePossessedStates(false));
+
         PossessionEnded.Invoke();
+    }
+
+    private void SetWhilePossessedStates(bool isPossessed)
+    {
+        foreach (GameObject obj in EnableWhilePossessedObjects)
+        {
+            obj.SetActive(isPossessed);
+        }
+
+        foreach (MonoBehaviour mono in EnableWhilePossessedComponents)
+        {
+            mono.enabled = isPossessed;
+        }
     }
 }
